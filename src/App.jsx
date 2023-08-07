f54e65f6f7de5806ea3b05f5e927542fbcfd61c4
jpentinio <joshuapentinio98@gmail.com>
2023-08-07 17:52:52 +0800

    add copyright

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,25 @@ function App() {
       <Navbar />
       <LandingPage />
       <Footer />
+      <div className="flex justify-center gap-1 p-2 text-sm">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io/home"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-lime-green"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.frontendmentor.io/profile/jpentinio"
+          target="_blank"
+          className="text-lime-green"
+        >
+          Joshua M. Pentinio
+        </a>
+      </div>
     </>
   );
 }
